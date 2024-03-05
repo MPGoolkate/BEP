752f861e0be99513f854c91be69ed95aaca12ef6
MPGoolkate <83120553+MPGoolkate@users.noreply.github.com>
2024-03-05 22:03:12 +0100

    get_neighbors weer aangepast + matching in array ipv graaf

--- unit_test_v2.py
+++ unit_test_v2.py
@@ -5,21 +5,25 @@ class Testing(unittest.TestCase):
 
     def test_normal_path(self):
         G = Graph()
+        M = Graph()
         for i in range(4):
             G.add_vertex(Vertex(i))
+            M.add_vertex(G.get_vertex(i))
 
         G.add_edge(G.get_vertex(0), G.get_vertex(1))
         G.add_edge(G.get_vertex(1), G.get_vertex(2))
         G.add_edge(G.get_vertex(3), G.get_vertex(2))
 
-        res = findMaxMatching(G)
+        res = findMaxMatching(G, M)
         
         self.assertEqual(res.count_matching()/2, 2)
 
     def test_normal_path2(self):
         G = Graph()
+        M = Graph()
         for i in range(4):
             G.add_vertex(Vertex(i))
+            M.add_vertex(G.get_vertex(i))
 
         G.add_edge(G.get_vertex(0), G.get_vertex(1))
         G.add_edge(G.get_vertex(1), G.get_vertex(2))
@@ -28,44 +32,50 @@ class Testing(unittest.TestCase):
 
         # M.add_vertex(G.get_vertex(1))
         # M.add_vertex(G.get_vertex(2))
-        G.matching = [-1, G.get_vertex(2), G.get_vertex(1), -1]
-        res2 = findMaxMatching(G)
+        M.add_edge(M.get_vertex(1), M.get_vertex(2))
+        res2 = findMaxMatching(G, M)
         self.assertEqual(res2.count_matching()/2, 2)
         
     
     def test_blossom_at_one_end(self):
         G = Graph()
+        M = Graph()
         for i in range(4):
             G.add_vertex(Vertex(i))
+            M.add_vertex(G.get_vertex(i))
 
         G.add_edge(G.get_vertex(0), G.get_vertex(1))
         G.add_edge(G.get_vertex(1), G.get_vertex(2))
         G.add_edge(G.get_vertex(1), G.get_vertex(3))
         G.add_edge(G.get_vertex(3), G.get_vertex(2))
 
-        res = findMaxMatching(G)
+        res = findMaxMatching(G, M)
         
         self.assertEqual(res.count_matching()/2, 2)
 
     def test_blossom_at_one_end2(self):
         G = Graph()
+        M = Graph()
         for i in range(4):
             G.add_vertex(Vertex(i))
+            M.add_vertex(G.get_vertex(i))
         G.add_edge(G.get_vertex(0), G.get_vertex(1))
         G.add_edge(G.get_vertex(1), G.get_vertex(2))
         G.add_edge(G.get_vertex(1), G.get_vertex(3))
         G.add_edge(G.get_vertex(3), G.get_vertex(2))
 
         # ensure that a blossom should occur
-        G.matching = [-1, G.get_vertex(2), G.get_vertex(1), -1]
-        res2 = findMaxMatching(G)
+        M.add_edge(M.get_vertex(1), M.get_vertex(2))
+        res2 = findMaxMatching(G, M)
         
         self.assertEqual(res2.count_matching()/2, 2)
 
     def test_blossom_at_both_ends(self):
         G = Graph()
+        M = Graph()
         for i in range(6):
             G.add_vertex(Vertex(i))
+            M.add_vertex(G.get_vertex(i))
 
         G.add_edge(G.get_vertex(0), G.get_vertex(1))
         G.add_edge(G.get_vertex(1), G.get_vertex(2))
@@ -75,15 +85,17 @@ class Testing(unittest.TestCase):
         G.add_edge(G.get_vertex(4), G.get_vertex(3))
         G.add_edge(G.get_vertex(4), G.get_vertex(5))
 
-        res = findMaxMatching(G)
+        res = findMaxMatching(G, M)
         
         self.assertEqual(res.count_matching()/2, 3)
 
     def test_blossom_at_both_ends2(self):
         # ensure that a blossom should occur
         G = Graph()
+        M = Graph()
         for i in range(6):
             G.add_vertex(Vertex(i))
+            M.add_vertex(G.get_vertex(i))
         
         G.add_edge(G.get_vertex(0), G.get_vertex(1))
         G.add_edge(G.get_vertex(1), G.get_vertex(2))
@@ -93,15 +105,17 @@ class Testing(unittest.TestCase):
         G.add_edge(G.get_vertex(4), G.get_vertex(3))
         G.add_edge(G.get_vertex(4), G.get_vertex(5))
 
-        G.matching = [G.get_vertex(1), G.get_vertex(0), -1, -1, -1, -1]
-        res2 = findMaxMatching(G)
+        M.add_edge(G.get_vertex(0), G.get_vertex(1))
+        res2 = findMaxMatching(G, M)
 
         self.assertEqual(res2.count_matching()/2, 3)
 
     def test_blossom_middle(self):
         G = Graph()
+        M = Graph()
         for i in range(8):
             G.add_vertex(Vertex(i))
+            M.add_vertex(G.get_vertex(i))
 
         G.add_edge(G.get_vertex(0), G.get_vertex(7))
         G.add_edge(G.get_vertex(1), G.get_vertex(2))
@@ -112,13 +126,15 @@ class Testing(unittest.TestCase):
         G.add_edge(G.get_vertex(5), G.get_vertex(6))
         G.add_edge(G.get_vertex(7), G.get_vertex(6))
 
-        res = findMaxMatching(G)
+        res = findMaxMatching(G, M)
         self.assertEqual(res.count_matching()/2, 4)
 
     def test_blossom_middle2(self):
         G = Graph()
+        M = Graph()
         for i in range(8):
             G.add_vertex(Vertex(i))
+            M.add_vertex(G.get_vertex(i))
 
         G.add_edge(G.get_vertex(0), G.get_vertex(7))
         G.add_edge(G.get_vertex(1), G.get_vertex(2))
@@ -131,15 +147,18 @@ class Testing(unittest.TestCase):
 
 
         # ensure that a blossom should occur
-        G.matching = [-1, -1, G.get_vertex(3), G.get_vertex(2), G.get_vertex(7), G.get_vertex(5), G.get_vertex(6), G.get_vertex(4)]
-
-        res2 = findMaxMatching(G)
+        M.add_edge(G.get_vertex(2), G.get_vertex(3))
+        M.add_edge(G.get_vertex(5), G.get_vertex(6))
+        M.add_edge(G.get_vertex(4), G.get_vertex(7))
+        res2 = findMaxMatching(G, M)
         self.assertEqual(res2.count_matching()/2, 4)
 
     def test_recursion_blossom(self):
         G = Graph()
+        M = Graph()
         for i in range(10):
             G.add_vertex(Vertex(i))
+            M.add_vertex(G.get_vertex(i))
 
         G.add_edge(G.get_vertex(0), G.get_vertex(1))
         G.add_edge(G.get_vertex(1), G.get_vertex(2))
@@ -153,14 +172,15 @@ class Testing(unittest.TestCase):
         G.add_edge(G.get_vertex(8), G.get_vertex(9))
         G.add_edge(G.get_vertex(9), G.get_vertex(6))
     
-        res = findMaxMatching(G)
+        res = findMaxMatching(G, M)
         self.assertEqual(res.count_matching()/2, 5)
     
     def test_recursion_blossom2(self):
         G = Graph()
-    
+        M = Graph()
         for i in range(10):
             G.add_vertex(Vertex(i))
+            M.add_vertex(G.get_vertex(i))
 
         G.add_edge(G.get_vertex(0), G.get_vertex(1))
         G.add_edge(G.get_vertex(1), G.get_vertex(2))
@@ -175,9 +195,11 @@ class Testing(unittest.TestCase):
         G.add_edge(G.get_vertex(9), G.get_vertex(6))
 
         # ensure that a blossom in a blossom should occur
-        G.matching = [-1, G.get_vertex(2), G.get_vertex(1), G.get_vertex(5), -1, G.get_vertex(3),
-                       G.get_vertex(9), G.get_vertex(8), G.get_vertex(7), G.get_vertex(6)]
-        res2 = findMaxMatching(G)
+        M.add_edge(G.get_vertex(1), G.get_vertex(2))
+        M.add_edge(G.get_vertex(5), G.get_vertex(3))
+        M.add_edge(G.get_vertex(7), G.get_vertex(8))
+        M.add_edge(G.get_vertex(6), G.get_vertex(9))
+        res2 = findMaxMatching(G, M)
         self.assertEqual(res2.count_matching()/2, 5)
 
 if __name__ == '__main__':
